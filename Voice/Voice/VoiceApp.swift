f9aeb77f7e3c973f60a2879b49a1a0fdd3ef000c
nbread-dev <ic.us@icloud.com>
2024-05-21 17:55:33 +0200

    Add .xcodeproj

---
+++ Voice/Voice/VoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceApp.swift
+//  Voice
+//
+//  Created by T.D.L Noble Break I.I on 5/21/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
